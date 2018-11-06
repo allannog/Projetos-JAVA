cd07782c18d0f69f377d5360d82b105d918af497
allannog <allannogsilva@hotmail.com>
2018-11-06 17:01:33 -0200

    Adicionado JAVA DOC

--- Banco/src/CaixaEletronico.java
+++ Banco/src/CaixaEletronico.java
@@ -1,30 +1,62 @@
 import java.text.NumberFormat;
 import java.util.Scanner;
 
+/**
+ * @version 1.0
+ * @author Kallan
+ *Aplicacao para uso da classe Conta
+ */
 public class CaixaEletronico {
 
 	public static void main(String[] args) {
 
+		/**
+		 * criado novo objeto minhaConta da classe Conta
+		 */
 		Conta minhaConta = new Conta();
 
+		/**
+		 * criado novo objeto entrada da classe Scanner para receber um valor
+		 */
 		Scanner entrada = new Scanner(System.in);
 
+		/**
+		 * atributo continua do tipo boolean com o valor true
+		 */
 		boolean continua = true;
 
+		/**
+		 * criado loop while que usa como parametro o boolean continua
+		 */
 		while (continua) {
 
+			/**
+			 * Enquanto a condição de while for verdadeira ele exibira as opções abaixo
+			 */
 			System.out.println("Selecione sua opção:");
 			System.out.println("1 - Saldo");
 			System.out.println("2 - Depósito");
 			System.out.println("3 - Saque");
 			System.out.println("4 - Encerrar");
 			System.out.print("Digite sua opção: ");
+			/**
+			 * Ao escolher uma das opções ela será armazenada em um int opcao que esta ligado ao Scanner entrada
+			 */
 			int opcao = entrada.nextInt();
 
+			/**
+			 * Esse int opção ira direcionar através do switch caso ele deve exibir 
+			 */
 			switch (opcao) {
 			
+			/**
+			 * caso 1 para saldo
+			 */
 			case 1:
 				
+				/**
+				 * criada uma instancia de NumberFormat para formatar em padrão de moeda brasileira o valor do método getSaldo()
+				 */
 				NumberFormat nf = NumberFormat.getCurrencyInstance();
 				String valorFormato = nf.format(minhaConta.getSaldo());
 				
@@ -32,11 +64,20 @@ public class CaixaEletronico {
 
 				break;
 
+				/**
+				 * caso 2 para depósito
+				 */
 			case 2:
 				
+				/**
+				 * será solicitado que seja digitado o valor a ser depositado
+				 */
 				System.out.print("Digite o valor do depósito: ");
 				double valorDepositado = entrada.nextDouble();
 				
+				/**
+				 * caso ocorra uma exceção o try ira tratar a mesma em conjunto com o catch
+				 */
 				try {
 					minhaConta.depositar(valorDepositado);
 				} catch (Exception e) {
@@ -45,11 +86,20 @@ public class CaixaEletronico {
 
 				break;
 
+				/**
+				 * caso 3 para saque
+				 */
 			case 3:
 				
+				/**
+				 * será solicitado que seja digitado o valor a ser depositado
+				 */
 				System.out.print("Digite o valor do saque: ");
 				double valorSacado = entrada.nextDouble();
 				
+				/**
+				 * caso ocorra uma exceção o try ira tratar a mesma em conjunto com o catch
+				 */
 				try {
 					minhaConta.sacar(valorSacado);
 				} catch (Exception e) {
@@ -58,13 +108,22 @@ public class CaixaEletronico {
 
 				break;
 
+				/**
+				 * caso 4 para encerrar
+				 */
 			case 4:
 				
+				/**
+				 * será exibida a mensagem abaixo e loop while usando continua como true será alterado para false e será encerrada a sessão
+				 */
 				System.out.println("Operação encerrada, obrigado!");
 				continua = false;
 
 				break;
 
+				/**
+				 * caso seja digitada alguma opção, fora das opções possiveis, será exibida a mensagem abaixo
+				 */
 			default:
 				System.out.println("Opção inválida");
 				break;
--- Banco/src/Conta.java
+++ Banco/src/Conta.java
@@ -1,36 +1,72 @@
-
+/**
+ *classe conta para que sejam instaciados objetos dessa classe
+ *@author Kallan
+ *@version 1.0
+ */
 public class Conta {
 
+	/**
+	 * atributo saldo do tipo double com visualização private
+	 */
 	private double saldo;
 
+	/**
+	 * método para realização de depósitos
+	 * @param valor recebe como parâmetro um valor do tipo double, é o valor a ser depositado
+	 * @throws Exception o método mostra uma mensagem de exceção caso a mesma ocorra
+	 */
 	public void depositar(double valor) throws Exception {
 
+		/**
+		 * se o valor informado como parâmetro for menor do que 0, será exibida a mensagem de exceção abaixo 
+		 */
 		if (valor < 0) {
 			throw new Exception("O valor informado é negativo");
 		}
 
+		/**
+		 * caso o valor informado não seja menor que zero o valor será somado ao saldo e exibirá a mensagem abaixo
+		 */
 		this.saldo += valor;
 
 		System.out.println("O depósito foi realizado com sucesso!");
 
 	}
 
+	/**
+	 * método para realização de saques
+	 * @param valor valor recebe como parâmetro um valor do tipo double, é o valor a ser sacado
+	 * @throws Exception o método mostra uma mensagem de exceção caso a mesma ocorra
+	 */
 	public void sacar(double valor) throws Exception {
 
+		/**
+		 * se o valor informado como parâmetro for menor do que 0, será exibida a mensagem de exceção abaixo 
+		 */
 		if (valor < 0) {
 			throw new Exception("O valor informado é negativo");
 		}
 
+		/**
+		 * se o valor informado como parâmetro for maior do que o saldo, será exibida a mensagem abaixo
+		 */
 		if (valor > saldo) {
 			throw new Exception("O saldo é insuficiente");
 		}
 
+		/**
+		 * caso o valor informado não seja menor que zero e maior que o saldo, o valor será subtraido do saldo e exibirá a mensagem abaixo
+		 */
 		this.saldo -= valor;
 
 		System.out.println("O saque foi realizado com sucesso!");
 
 	}
 
+	/**
+	 * método para retornar o valor do saldo
+	 * @return retona do valor do saldo
+	 */
 	public double getSaldo() {
 		return saldo;
 	}
